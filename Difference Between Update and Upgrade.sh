e0b503b901c2288c25228f49f808b0817ed96dd0
kavitha <kavitha.chigurlapalli@gmail.com>
2024-06-29 15:36:05 +0530

    added modified files

---
+++ Difference Between Update and Upgrade.sh
@@ -0,0 +1,2 @@
+Update: Refreshes the list of available packages and versions but does not install or upgrade any packages.
+Upgrade: Installs the latest versions of all packages currently installed on the system.
